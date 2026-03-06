3ab69bb2ba98964963c25fc93f86da1900b7df81
WillyChenInsighteye <willy.chen@insighteye.com.tw>
2026-03-06 11:35:36 +0800

    feat: update prouter

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory} from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 import Campaign from '../pages/Campaign.vue';
 
 const routes = [
@@ -55,7 +55,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
